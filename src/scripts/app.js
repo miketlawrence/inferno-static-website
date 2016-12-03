de900478a8f0c1e047b54c83e12cc315f075f65c
Luke Edwards <luke.edwards05@gmail.com>
2016-12-03 12:00:30 -0800

    move onScroller func to partial

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,6 @@
 'use strict';
 
 var debounce = require('debounce');
+var scroller = require('./scroller');
 
-var ran = 0;
-var bench = document.querySelector('.bench');
-// `.bench` should hit 65% of win.height, from bottom
-var target = Math.round(bench.offsetTop - window.innerHeight * 0.35);
-
-var bars = [].slice.call(document.getElementsByClassName('bar'));
-
-var widths = bars.map(function (el) {
-	return el.getAttribute('width');
-});
-
-function onScroll() {
-	if (!ran && window.scrollY >= target) {
-		ran = 1;
-		bars.forEach(function (el, idx) {
-			el.style.width = widths[idx];
-		});
-	}
-}
-
-window.onscroll = debounce(onScroll, 120);
+window.onscroll = debounce(scroller, 120);
---
+++ src/scripts/scroller.js
@@ -0,0 +1,23 @@
+/**
+ * OnScroll: Animate Bar Chart
+ */
+
+var ran = 0;
+var bench = document.querySelector('.bench');
+// `.bench` should hit 65% of win.height, from bottom
+var target = Math.round(bench.offsetTop - window.innerHeight * 0.35);
+
+var bars = [].slice.call(document.getElementsByClassName('bar'));
+
+var widths = bars.map(function (el) {
+	return el.getAttribute('width');
+});
+
+module.exports = function () {
+	if (!ran && window.scrollY >= target) {
+		ran = 1;
+		bars.forEach(function (el, idx) {
+			el.style.width = widths[idx];
+		});
+	}
+}
