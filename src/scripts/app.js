66918542dd71a801474f94858b5887bafd5a829b
Luke Edwards <luke.edwards05@gmail.com>
2016-12-08 14:51:31 -0800

    animate bars onload, if viewing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,6 +3,7 @@
 var debounce = require('debounce');
 var scroller = require('./scroller');
 
+window.onload = scroller; // check onload
 window.onscroll = debounce(scroller, 120);
 
 require('./tabs');
